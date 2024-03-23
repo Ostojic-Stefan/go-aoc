e592a82983f4d94513779c1a5189cb113079e77c
name <ostojicstefan1337@gmail.com>
2024-03-23 14:49:19 +0100

    day12 part2

--- 2020/day12/main.go
+++ 2020/day12/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"goaoc/utils"
 	"log"
+	"math"
 	"strconv"
 )
 
@@ -69,6 +70,54 @@ func part1() {
 	fmt.Println(s.xPos + s.yPos)
 }
 
+func part2() {
+	s := state{10, 1, DIR_EAST}
+	shipx, shipy := 0, 0
+	instructions := map[string]func(int){}
+	instructions["N"] = func(i int) {
+		s.yPos += i
+	}
+	instructions["S"] = func(i int) {
+		s.yPos -= i
+	}
+	instructions["E"] = func(i int) {
+		s.xPos += i
+	}
+	instructions["W"] = func(i int) {
+		s.xPos -= i
+	}
+	instructions["L"] = func(i int) {
+		if i == 90 {
+			s.xPos, s.yPos = -s.yPos, s.xPos
+		} else if i == 180 {
+			s.xPos, s.yPos = -s.xPos, -s.yPos
+		} else {
+			s.xPos, s.yPos = s.yPos, -s.xPos
+		}
+	}
+	instructions["R"] = func(i int) {
+		if i == 90 {
+			s.xPos, s.yPos = s.yPos, -s.xPos
+		} else if i == 180 {
+			s.xPos, s.yPos = -s.xPos, -s.yPos
+		} else {
+			s.xPos, s.yPos = -s.yPos, s.xPos
+		}
+	}
+	instructions["F"] = func(i int) {
+		shipx += i * s.xPos
+		shipy += i * s.yPos
+	}
+	lines := utils.ReadLinesAsString("input.txt")
+	for _, line := range lines {
+		num, _ := strconv.Atoi(line[1:])
+		op := line[0:1]
+		instructions[op](num)
+	}
+	fmt.Println(math.Abs(float64(shipx)) + math.Abs(float64(shipy)))
+}
+
 func main() {
-	part1()
+	// part1()
+	part2()
 }
